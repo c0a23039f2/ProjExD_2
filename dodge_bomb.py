4d1ca7a1b443ea706c12b6372747a60e931555f2
c0a23039 <c0a23039f2@edu.teu.ac.jp>
2025-09-30 14:37:30 +0900

    en1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -12,6 +13,43 @@ DELTA = {
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値: 判定結果タプル（よこ, 縦）
+    画面内ならTrue/画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
+def gameover(screen: pg.Surface) -> None:
+     gmover_img = pg.Surface((WIDTH,HEIGHT))#gmoverの画面
+     pg.draw.rect(gmover_img, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
+     gmover_img.set_alpha(128)
+     screen.blit(gmover_img, (0,0))
+
+     fonto = pg.font.Font(None, 80)
+     txt = fonto.render("Game Over", True, (255, 255, 255))
+     txt_rect = txt.get_rect(center=(WIDTH/2, HEIGHT/2))
+     screen.blit(txt, txt_rect)
+
+     go_img = pg.image.load("fig/8.png")
+     left_img_rect = go_img.get_rect(center=(WIDTH/4, HEIGHT/2))
+     right_img_rect = go_img.get_rect(center=(WIDTH*3/4, HEIGHT/2))
+
+     screen.blit(go_img, left_img_rect)
+     screen.blit(go_img, right_img_rect)
+
+
+
+
+     pg.display.update()
+
 
 
 def main():
@@ -35,9 +73,13 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾の衝突判定
+            gameover(screen)
+            time.sleep(5)
+            return
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
+        sum_mv  = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  # 横方向の移動量を加算
@@ -53,8 +95,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)  # 爆弾移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx*= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)  # 爆弾描画
         pg.display.update()
         tmr += 1
